d7b7e4e1aa548b8e311a0ab8cc7843c6fc84ebea
Diego Lemos <diegolemosderesende@gmail.com>
2018-05-10 23:22:35 +0100

    First scratch of a parser function

---
+++ parser/parser.go
@@ -0,0 +1,23 @@
+package parser
+
+import (
+	"fmt"
+	"regexp"
+	"time"
+)
+
+type LogEvent struct {
+	Level, Message string
+	Date           time.Time
+}
+
+func Parse(logEvent string) LogEvent {
+	fmt.Printf(logEvent)
+	rabbitmqPattern := regexp.MustCompile(`(?s)^=(.*)====\s(.*)\s===\n(.*)`)
+	if rabbitmqPattern.MatchString(logEvent) {
+		logData := rabbitmqPattern.FindStringSubmatch(logEvent)
+		logEventTime, _ := time.Parse("2-Jan-2006::15:4:5", logData[2])
+		return LogEvent{logData[1], logData[3], logEventTime}
+	}
+	return LogEvent{}
+}
---
+++ parser/parser_test.go
@@ -0,0 +1,26 @@
+package parser_test
+
+import (
+	. "logstreamer/parser"
+
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Parser", func() {
+	It("should parse RabbitMQ log events", func() {
+		rabbitmqLogEvent := `=WARNING REPORT==== 5-May-2018::16:27:16 ===
+global: rabbit@6b44fa337952635b266d4cda4f1fb908 failed to connect to rabbit@8063b04b5406b66334951c641fe9f634`
+		Expect(Parse(rabbitmqLogEvent)).To(Equal(LogEvent{"WARNING REPORT", "global: rabbit@6b44fa337952635b266d4cda4f1fb908 failed to connect to rabbit@8063b04b5406b66334951c641fe9f634", time.Date(2018, time.May, 5, 16, 27, 16, 0, time.UTC)}))
+	})
+
+	It("should parse RabbitMQ log events with multiple lines", func() {
+		rabbitmqLogEvent := `=INFO REPORT==== 5-May-2018::16:26:53 ===
+FHC read buffering:  OFF
+FHC write buffering: ON
+`
+		Expect(Parse(rabbitmqLogEvent)).To(Equal(LogEvent{"INFO REPORT", "FHC read buffering:  OFF\nFHC write buffering: ON\n", time.Date(2018, time.May, 5, 16, 26, 53, 0, time.UTC)}))
+	})
+})
