82e23134c7aa5b5fb65be8183e26b13c34c61d9c
Diego Lemos <diegolemosderesende@gmail.com>
2018-05-13 22:39:26 +0100

    Remove logs, too noisy

--- log/reader.go
+++ log/reader.go
@@ -1,7 +1,6 @@
 package log
 
 import (
-	golog "log"
 	"regexp"
 )
 
@@ -10,15 +9,11 @@ func ChopLogEvent(data []byte, atEOF bool) (advance int, token []byte, err error
 	rabbitmqPattern := regexp.MustCompile(`^(?:[[:space:]]|[A-Za-z]|\*)*(=(.*)====\s(.*)\s===\n((?:.+\n)+)\n)`)
 	text := string(data)
 
-	golog.Printf("Data chunk:\n%v\n", text)
-
 	if rabbitmqPattern.MatchString(text) {
 		groups := rabbitmqPattern.FindStringSubmatch(text)
 		logEvent := groups[1]
-		golog.Printf("Found match:\n%v\n", logEvent)
 		return len([]byte(groups[0])), []byte(logEvent), nil
 	}
 
-	golog.Println("No match found")
 	return 0, nil, nil
 }
